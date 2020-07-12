33d7329bf003f286656b40efd4725d29f46c04ef
Christopher Crookes <cacrookes@gmail.com>
2020-07-11 22:41:56 -0700

    Adding didPlayerWin function.

--- Roshambo Assignment/ChooseGestureViewController.swift
+++ Roshambo Assignment/ChooseGestureViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ChooseGestureViewController: UIViewController {
 
+    enum Hand: Int {
+        case rock = 0, paper, scissors
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    /// Randomly generates an iteger from 0 to 2 to represent the opponents move
-    /// - Returns: An Int with a value of either 0, 1, or 2.
-    func generateOpponentPlay() -> Int {
-        return Int.random(in: 0...2)
+    /// Randomly generates an iteger from 0 to 2 to represent the opponents move, and returns the associated hand.
+    /// - Returns: the opponents hand, either rock, paper, or scissors
+    func generateOpponentPlay() -> Hand {
+        return Hand(rawValue: Int.random(in: 0...2))!
+    }
+    
+    /// Checks if the player beat the opponent.
+    /// - Parameters:
+    ///   - playerMove: Hand type that represents the players move, either rock, paper, or scissors
+    ///   - opponentMove: Hand type that represents the opponents move, either rock, paper, or scissors
+    /// - Returns: Bool, indicating if the player won or not. Ties and losses are returned as false.
+    func didPlayerWin(playerMove: Hand, opponentMove: Hand) -> Bool {
+        switch playerMove {
+        case .rock:
+            return opponentMove == Hand.scissors ? true : false
+        case .paper:
+            return opponentMove == Hand.rock ? true : false
+        case .scissors:
+            return opponentMove == Hand.paper ? true : false
+        }
     }
     
 }
