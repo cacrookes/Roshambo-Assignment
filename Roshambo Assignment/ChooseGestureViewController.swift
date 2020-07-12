fc4791f356f71faa9fac59fa4008d6e340ae470a
Christopher Crookes <cacrookes@gmail.com>
2020-07-11 22:22:15 -0700

    Adding generateOpponentMove function.

--- Roshambo Assignment/ChooseGestureViewController.swift
+++ Roshambo Assignment/ChooseGestureViewController.swift
@@ -15,6 +15,11 @@ class ChooseGestureViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    /// Randomly generates an iteger from 0 to 2 to represent the opponents move
+    /// - Returns: An Int with a value of either 0, 1, or 2.
+    func generateOpponentPlay() -> Int {
+        return Int.random(in: 0...2)
+    }
+    
 }
 
