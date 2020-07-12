d61571180969d747193403b697a4f4c063808516
Christopher Crookes <cacrookes@gmail.com>
2020-07-11 22:14:04 -0700

    Adding ResultsViewController.

---
+++ Roshambo Assignment/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Roshambo Assignment
+//
+//  Created by Christopher Crookes on 2020-07-11.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
